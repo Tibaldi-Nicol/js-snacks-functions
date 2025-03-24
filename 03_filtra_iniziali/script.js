f0b2998a96fb5f9b1425a4b662f38a7a4895d53d
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-24 18:13:55 +0100

    [Done]-3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,9 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtraIniziali(array, lettera) {
+    let iniziali = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].charAt(0) === lettera) {
+            iniziali.push(array[i]);
+        }
+    }
+    return iniziali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(filtraIniziali(names, 'A'));
 
 
 
